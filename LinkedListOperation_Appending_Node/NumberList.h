f38624d9745fe811653d11db287f19f88587b243
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-11 18:00:09 -0500

    append node in he end

---
+++ LinkedListOperation_Appending_Node/NumberList.h
@@ -0,0 +1,33 @@
+#ifndef NUMBERLIST_H
+#define NUMBERLIST_H
+#include <cstddef>
+
+class NumberList
+{
+    private:
+            struct ListNode
+            {
+                double value; //The value in the node
+                struct ListNode *next; //self Referencial data structure, points to the next node
+            };
+
+            ListNode* head; //List head pointer
+    public:
+            NumberList()
+            {
+                head = NULL;
+            }
+
+            //destructor
+            ~NumberList()
+            {};
+
+            //LinkedList operations
+            void appendNode(double);
+            void insertNode(double);
+            void deleteNode(double);
+            void displayNode(double);
+    
+};
+
+#endif
