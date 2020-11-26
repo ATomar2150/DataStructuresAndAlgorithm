349e7b6e878f08ef075a59b46d35ab799cc9dc4d
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-25 22:21:51 -0500

    Added delete function

--- OperationOnArrayByAbdulBari/main.cpp
+++ OperationOnArrayByAbdulBari/main.cpp
@@ -41,21 +41,48 @@ void insert(struct Array* arr, int index, int num)
     }
 };
 
+int Delete(struct Array* arr, int index)
+{
+
+    int x = 0;
+    int i;
+
+    if(index >= 0 && index < arr->length)
+    {
+        x = arr->A[index];
+
+        for(int i = index; i < arr->length-1; i++)
+        {
+            arr->A[i] = arr->A[i+1];
+        }
+
+        arr->length--;
+        return x;
+    }
+    return 0; //if index is invalid
+};
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
     display(arr);
     
-    cout << "\n\nAppending one element at the End." <<endl;
+    cout << "\n\nAppending one element at the End." << endl;
 
     append(&arr, 13);
 
     display(arr);
 
-    cout << "\n\nInserting one element at the given Index." <<endl;
+    cout << "\n\nInserting one element at the given Index." << endl;
 
     insert(&arr, 2, 1); //(struct Array* arr, int index, int num)
+
+    display(arr);
+
+    cout << "\n\nDeleting Element from an array at particular Index." << endl;
+
+    cout<<"Deleted element is: " << Delete(&arr, 3) << endl; //(struct Array* arr, int index)
 
     display(arr);
 
