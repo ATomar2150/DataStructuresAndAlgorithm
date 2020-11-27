85abb61fa8492bcaac949361c19b583d2a8e26a6
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-27 02:56:51 -0500

    Rearranged positive elements one side and negative elements other side

--- AbdulBari_SortedArray/main.cpp
+++ AbdulBari_SortedArray/main.cpp
@@ -33,6 +33,14 @@ void insertElement(struct Array* arr, int num)
     arr->length++;
 };
 
+void swap(int* A, int* B)
+{
+    int temp;
+    temp = *A;
+    *A = *B;
+    *B = temp;
+};
+
 int isSorted(struct Array arr)
 {
     for (int i = 0; i < arr.length-1; i++)
@@ -43,17 +51,45 @@ int isSorted(struct Array arr)
         }
     }
     return 1;
-}
+};
+
+void reArrange(struct Array* arr)
+{
+    int i,j;
+    i = 0;
+    j = arr->length-1;
+    while(i < j)
+    {
+        while(arr->A[i] < 0)
+        {
+            i++;
+        }
+        while(arr->A[j] > 0)
+        {
+            j--;
+        }
+        if(i<j)
+        {
+            swap(&arr->A[i], &arr->A[j]);
+        }
+    
+    }
+    
+};
 
 
 int main()
 {
-    struct Array arr = {{3, 4, 6, 8, 9}, 5, 11};
+    struct Array arr = {{3, 4, 6, -8, -9}, 5, 11};
+
+    reArrange(&arr);
+    
+    displayElement(arr);
+    cout <<endl;
 
     insertElement(&arr, 7);
 
     displayElement(arr);
-
     cout <<"\nIt checks whether the array is sorted or not, return 1 if sorted & return 0 if not sorted: ";
     cout << isSorted(arr);
 
