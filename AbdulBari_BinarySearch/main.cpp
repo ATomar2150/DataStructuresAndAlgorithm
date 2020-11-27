a6cbbe1596b97a405c97a059bd796e84cdad5f0c
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-26 19:06:40 -0500

    Binary Saerch Through recursion added

--- AbdulBari_BinarySearch/main.cpp
+++ AbdulBari_BinarySearch/main.cpp
@@ -1,3 +1,54 @@
+// #include <iostream>
+// using namespace std;
+
+// struct Array
+// {
+//     int A[10];
+//     int length;
+//     int size;
+// };
+
+// int binarySearch(struct Array arr, int num)
+// {
+//     int low, mid, high;
+//     int i = 0;
+//     high = arr.length-1;
+//     low = 0;
+
+//     while(low <= high)
+//     {
+//         mid = (low + high) / 2;
+//         if(num == arr.A[mid])
+//         {
+//             return mid;
+//         }
+//         else if( num < arr.A[mid] )
+//         {
+//             high = mid - 1;
+//         }
+//         else if( num > arr.A[mid] )
+//         {
+//             low = mid + 1;
+//         }
+//         else
+//         {
+//             return -1;
+//         }
+        
+//     }
+// }
+
+// int main()
+// {
+//     struct Array arr = {{2,3,4,6,7,9}, 6, 10};
+
+//     cout<<"The index is at: "<< binarySearch(arr, 7) << endl;
+
+//     return 0;
+// } 
+
+//RECURSIVE VERSION OF BINARY SEARCH
+
 #include <iostream>
 using namespace std;
 
@@ -8,27 +59,23 @@ struct Array
     int size;
 };
 
-int binarySearch(struct Array arr, int num)
+int RecursiveSearch(int a[], int low, int high, int num)
 {
-    int low, mid, high;
-    int i = 0;
-    high = arr.length-1;
-    low = 0;
-
-    while(low <= high)
+    int mid;
+    if (low <= high)
     {
         mid = (low + high) / 2;
-        if(num == arr.A[mid])
+        if(num == a[mid])
         {
             return mid;
         }
-        else if( num < arr.A[mid] )
+        else if(num < a[mid])
         {
-            high = mid - 1;
+            return RecursiveSearch(a, low, mid - 1, num);
         }
-        else if( num > arr.A[mid] )
+        else if(num > a[mid])
         {
-            low = mid + 1;
+            return RecursiveSearch(a, mid + 1 , high, num);
         }
         else
         {
@@ -36,13 +83,14 @@ int binarySearch(struct Array arr, int num)
         }
         
     }
+    
 }
 
 int main()
 {
     struct Array arr = {{2,3,4,6,7,9}, 6, 10};
 
-    cout<<"The index is at: "<< binarySearch(arr, 7) << endl;
+    cout<<"The index is at: "<< RecursiveSearch(arr.A, 0, arr.length-1, 11)  << endl;
 
     return 0;
 } 
