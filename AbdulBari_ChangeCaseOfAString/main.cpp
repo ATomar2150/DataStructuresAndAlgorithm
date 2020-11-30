ec077ecb21b3111d9690c19ab6ece38a385ce6fe
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-30 16:59:29 -0500

    changing case of string validation added

--- AbdulBari_ChangeCaseOfAString/main.cpp
+++ AbdulBari_ChangeCaseOfAString/main.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main()
 {
-   char A[] = "WELCOME";
+   char A[] = "WeLCOME";
 
    for(int i = 0; A[i] != '\0'; i++)
    {
-       if( A[i] >= 65 && A[i] <= 90)
+       if(A[i] >= 65 && A[i] <= 90)
        {
        A[i] = A[i] + 32;
        cout << A[i];
+       }
+       else if(A[i] >= 97 && A[i] <= 122)
+       {
+           A[i] = A[i] - 32;
+           cout << A[i];
        }
    }
     return 0;
