f5c74aad55c21a99c26466e7328c62e64d55800d
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-30 16:56:10 -0500

    changing case of string

--- AbdulBari_ChangeCaseOfAString/main.cpp
+++ AbdulBari_ChangeCaseOfAString/main.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-   
+   char A[] = "WELCOME";
+
+   for(int i = 0; A[i] != '\0'; i++)
+   {
+       A[i] = A[i] + 32;
+       cout << A[i];
+   }
+
+
     return 0;
 }
