0eecda8f048473a2fa9dd1650b5088461aed2767
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-30 18:16:21 -0500

    Method1: reversing array

--- AbdulBari_ReversingAString/main.cpp
+++ AbdulBari_ReversingAString/main.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 int main()
 {
+   char A[] = "Python";
+
    
+    int len = 0;
+
+    for(int i = 0; A[i] != '\0'; i++)
+    {
+        len++;
+    }
+
+    cout <<"Length of the string is: "<< len <<endl;
+
+    char B[len+1];
+
+    int i, j;
+
+    for(i = 0; A[i] != '\0'; i++)
+    {
+
+    }
+    i = i - 1;
+
+    for(j = 0; i >= 0 ; i--, j++)
+    {
+        B[j] = A[i];
+    }
+
+    B[j] = '\0';
+
+    cout << B;
+    
     return 0;
 }
