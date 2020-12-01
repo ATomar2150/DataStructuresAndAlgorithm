cb1ff07ba228018d72ea1ab72df86c78f82e5b01
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-11-30 21:00:52 -0500

    Duplicate Checking Using Same Array

--- AbdulBari_FindingDuplicateInAString/main.cpp
+++ AbdulBari_FindingDuplicateInAString/main.cpp
@@ -3,6 +3,34 @@ using namespace std;
 
 int main()
 {
-   
+   char A[] = "APARNA";
+   int count = 0;
+
+  for(int i = 0; A[i] != '\0'; i++)
+  {
+      for(int j = 0; A[j] != '\0'; j++)
+      {
+          if(A[i] == A[j])
+          {
+              count++;
+          }
+          
+      }
+      cout << "The element is: "<< A[i] <<endl;
+     
+      if(count > 1)
+      {
+          cout <<"This character has a Duplicate.\n";
+          cout << "The frequency with which it is repeating is: "<<count <<endl <<endl;;
+      }
+      else
+      {
+          cout << "No Duplicate found.\n\n";
+      }
+      
+      count = 0;
+
+  }
+
     return 0;
 }
