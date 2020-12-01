668c183424fbb0d8b4edbbe799ca89264e2c0fff
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-12-01 00:14:39 -0500

    Hash Table

---
+++ AbdulBari_Anagram/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+bool anagram(int H[],int num)
+{
+     for(int i = 0; H[i] < num; i++)
+     {
+         if(H[i] != 0)
+         {
+            return false;
+         }
+     }
+     return true;
+};
+
+int main()
+{
+    char A[] = "deckmal";
+    char B[] = "medical";
+    int H[26] = {0};
+    int i;
+
+    for(int i = 0; A[i] != '\0'; i++)
+    {
+        H[A[i] - 97]++;
+    }
+
+    for(int i = 0; B[i] != '\0'; i++)
+    {
+        H[B[i] - 97]--;
+    }
+
+    if(anagram(H, 26))
+    {
+        cout<< "It is an Anagram";
+    }
+    else
+    {
+         cout<< "It is not an Anagram";
+    }
+    
+
+    return 0;
+}
